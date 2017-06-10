ac3006d501b978dc00fcab05cfc0f701a1c97058
Peter Georgiev <peter_85@abv.bg>
2017-06-10 15:03:00 +0300

    Completed footer

---
+++ gulp/tasks/sprite.js
@@ -0,0 +1,20 @@
+let gulp = require('gulp'),
+    svgSprite = require('gulp-svg-sprite');
+let config = {
+    mode: {
+        css: {
+            render: {
+                css: {
+                    template: './gulp/templates/sprite.css'
+                }
+            }
+
+        }
+    }
+}
+
+gulp.task('createSprite', function() {
+    return gulp.src('./app/assets/images/icons/**/*.svg')
+        .pipe(svgSprite(config))
+        .pipe(gulp.dest('./app/temp/sprite/'));
+})
